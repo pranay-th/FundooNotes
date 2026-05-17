1d925ff9f14c95ff54517d9edd7cc08c91c40e05
Pranay <pranayisgreat11@gmail.com>
2026-05-17 20:44:38 +0530

    Implemented Login Serializer for token matching

--- users/serializers.py
+++ users/serializers.py
@@ -29,3 +29,29 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+
+    password = serializers.CharField(write_only=True)
+
+    def validate(self,data):
+        username=data.get('username')
+        password=data.get('password')
+
+        user=authenticate(
+            username=username,
+            password=password
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid Credentials"
+            )
+
+        refresh=RefreshToken.for_user(user)
+
+        return{
+            'refresh':str(refresh),
+            'access':str(refresh.access_token),
+        }
